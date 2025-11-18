03b6ddedba64bfcf56c16f02aef4a19c9e58bed5
Hty <15161656368@163.com>
2025-11-18 20:43:23 +0800

    提交文件

--- user-center/src/main/java/com/hty/usercenter/UserCenterApplication.java
+++ user-center/src/main/java/com/hty/usercenter/UserCenterApplication.java
@@ -1,7 +1,10 @@
 package com.hty.usercenter;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@MapperScan("com.hty.usercenter.mapper")
 
 @SpringBootApplication
 public class UserCenterApplication {
---
+++ user-center/src/main/java/com/hty/usercenter/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.hty.usercenter.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.hty.usercenter.model.User;
+
+public interface UserMapper extends BaseMapper<User> {
+
+}
