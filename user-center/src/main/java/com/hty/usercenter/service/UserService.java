596c6ed40c4a577c798ae8d7e2041a8bd0f971f4
Hty <15161656368@163.com>
2025-11-20 09:10:44 +0800

    提交文件

--- user-center/src/main/java/com/hty/usercenter/service/UserService.java
+++ user-center/src/main/java/com/hty/usercenter/service/UserService.java
@@ -4,10 +4,17 @@ import com.hty.usercenter.model.domain.User;
 import com.baomidou.mybatisplus.extension.service.IService;
 
 /**
-* @author 33038
+* @author hty
 * @description 针对表【user(用户)】的数据库操作Service
 * @createDate 2025-11-19 11:00:34
 */
 public interface UserService extends IService<User> {
-
+    /**
+     * 用户注册
+     * @param userAccount 用户账户
+     * @param userPassword 用户密码
+     * @param checkPassword 校验密码
+     * @return 新用户id
+     */
+    long userRegister(String userAccount, String userPassword, String checkPassword);
 }
--- user-center/src/main/java/com/hty/usercenter/service/impl/UserServiceImpl.java
+++ user-center/src/main/java/com/hty/usercenter/service/impl/UserServiceImpl.java
@@ -1,13 +1,20 @@
 package com.hty.usercenter.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hty.usercenter.model.domain.User;
 import com.hty.usercenter.service.UserService;
 import com.hty.usercenter.mapper.UserMapper;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.util.DigestUtils;
+
+import javax.annotation.Resource;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
-* @author 33038
+* @author hty
 * @description 针对表【user(用户)】的数据库操作Service实现
 * @createDate 2025-11-19 11:00:34
 */
@@ -15,6 +22,54 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl extends ServiceImpl<UserMapper, User>
     implements UserService{
 
+    @Resource
+    private UserMapper userMapper;
+
+    @Override
+    public long userRegister(String userAccount, String userPassword, String checkPassword) {
+        //1、校验
+        if (StringUtils.isAnyBlank(userAccount, userPassword, checkPassword)){
+            return -1;
+        }
+        if (userAccount.length() < 4){
+            return -1;
+        }
+        if (userPassword.length() < 8 || checkPassword.length() < 8){
+            return -1;
+        }
+        //账户不能包含特殊字符
+        String validPattern = "\\pP|\\pS|\\s+";
+        Matcher matcher = Pattern.compile(validPattern).matcher(userAccount);
+        if (!matcher.find()){
+            return -1;
+        }
+        //密码和校验密码相同
+        if (!userPassword.equals(checkPassword)){
+            return -1;
+        }
+        //账户不能重复
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("userAccount",userAccount);
+        long count = userMapper.selectCount(queryWrapper);
+        if (count > 0){
+            return -1;
+        }
+
+        //2、加密
+        final String SALT = "hty";
+        String encryptPassword = DigestUtils.md5DigestAsHex((SALT+userPassword).getBytes());
+
+        //3、插入数据
+        User user = new User();
+        user.setUserAccount(userAccount);
+        user.setUserPassword(encryptPassword);
+
+        boolean saveResult = this.save(user);
+        if (!saveResult){
+            return -1;
+        }
+        return user.getId();
+    }
 }
 
 
