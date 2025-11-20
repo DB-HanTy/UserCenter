b23688360e74418b77ded603cc9f8d73135c9d6f
Hty <15161656368@163.com>
2025-11-20 10:43:22 +0800

    提交文件

--- user-center/src/main/java/com/hty/usercenter/service/UserService.java
+++ user-center/src/main/java/com/hty/usercenter/service/UserService.java
@@ -4,17 +4,27 @@ import com.hty.usercenter.model.domain.User;
 import com.baomidou.mybatisplus.extension.service.IService;
 
 /**
-* @author hty
-* @description 针对表【user(用户)】的数据库操作Service
-* @createDate 2025-11-19 11:00:34
-*/
+ * @author hty
+ * @description 针对表【user(用户)】的数据库操作Service
+ * @createDate 2025-11-19 11:00:34
+ */
 public interface UserService extends IService<User> {
     /**
      * 用户注册
-     * @param userAccount 用户账户
-     * @param userPassword 用户密码
+     *
+     * @param userAccount   用户账户
+     * @param userPassword  用户密码
      * @param checkPassword 校验密码
      * @return 新用户id
      */
     long userRegister(String userAccount, String userPassword, String checkPassword);
+
+    /**
+     * 登录
+     * @param userAccount  用户账户
+     * @param userPassword 用户密码
+     * @return 脱敏后的用户信息
+     */
+    User doLogin(String userAccount, String userPassword);
+
 }
